94d7c84a3ef6c5cb798548cc6c7f306cf4bb83f0
imaijin <jin.imai@kk-sanwa-ss.co.jp>
2025-09-09 18:34:08 +0900

    Add apps.py

--- myproject/myapp/apps.py
+++ myproject/myapp/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 # このアプリ（myapp）の設定クラス
 class MyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'  # デフォルト主キー型
-    name = 'myapp'  # アプリ名
+    name = 'myproject.myapp'  # アプリ名
